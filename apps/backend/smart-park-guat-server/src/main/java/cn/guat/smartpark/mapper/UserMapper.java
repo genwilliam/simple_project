6e356acb3cf81206a7301697868ce637e3774075
william <genwillen@gmail.com>
2025-11-07 23:18:40 +0800

    优化后端sql语句

--- apps/backend/smart-park-guat-server/src/main/java/cn/guat/smartpark/mapper/UserMapper.java
+++ apps/backend/smart-park-guat-server/src/main/java/cn/guat/smartpark/mapper/UserMapper.java
@@ -8,21 +8,21 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    @Select("SELECT count(1) FROM `user` WHERE user_name=#{userName} AND password=#{password}")
+    @Select("SELECT count(1) FROM `charging_user` WHERE user_name=#{userName} AND password=#{password}")
     public Boolean checkLogin(User user);
 
-    @Select("SELECT count(1) FROM `user` WHERE user_name=#{userName} limit 1")
+    @Select("SELECT count(1) FROM `charging_user` WHERE user_name=#{userName} limit 1")
     public Boolean checkAdd(User user);
 
-    @Select("SELECT * FROM `user`")
+    @Select("SELECT * FROM `charging_user`")
     public List<User> list();
 
-    @Insert("INSERT INTO `user`(user_name,password,phone_number,email,sex,status) VALUES(#{userName},#{password},#{phoneNumber},#{email},#{sex},#{status})")
+    @Insert("INSERT INTO `charging_user`(user_name,password,phone,email,sex,status) VALUES(#{userName},#{password},#{phoneNumber},#{email},#{sex},#{status})")
     public Boolean add(User user);
 
-    @Update("UPDATE `user` SET password=#{password},phone_number=#{phoneNumber},email=#{email},sex=#{sex},status=#{status} WHERE id=#{id}")
+    @Update("UPDATE `charging_user` SET password=#{password},phone_number=#{phoneNumber},email=#{email},sex=#{sex},status=#{status} WHERE id=#{id}")
     public Boolean edit(User user);
 
-    @Delete("DELETE FROM `user` WHERE id=#{id}")
+    @Delete("DELETE FROM `charging_user` WHERE id=#{id}")
     public Boolean delete(Long id);
 }
